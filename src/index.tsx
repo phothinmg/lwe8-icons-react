99954cfaa87ce0269292863b31c1ce4aa8b8c71a
phothinmg <phothinmg@gmail.com>
2025-01-18 19:23:43 +0630

    modified:   src/icons.ts

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import type { IconName, Icons, JsxSocialIconProps } from "./types.js";
 export type { IconName, JsxSocialIconProps };
 
 const getPathd = (name: IconName): string => {
-	const pd = icons.find((i) => i.name === name) as Icons;
-	return pd.d;
+  const pd = icons.find((i) => i.name === name) as Icons;
+  return pd.d;
 };
 
 /**
@@ -48,65 +48,65 @@ const getPathd = (name: IconName): string => {
  */
 
 export default function ReactIcon({
-	name,
-	href = "#",
-	target = "_blank",
-	fillColor = "currentColor",
-	fillOpacity = 0.7,
-	className = "",
-	size = 24,
-	title = name,
+  name,
+  href = "#",
+  target = "_blank",
+  fillColor = "currentColor",
+  fillOpacity = 0.7,
+  className = "",
+  size = 24,
+  title = name,
 }: JsxSocialIconProps): JSX.Element {
-	const [pathd, setPathd] = useState<string>(() => getPathd(name));
-	const [opacity, setOpacity] = useState<number>(fillOpacity);
-	const [color, setColor] = useState<string>(fillColor);
-	const [link, setLink] = useState<string>(href);
-	const [tg, setTg] = useState<string>(target);
-	const [cname, setCname] = useState<string>(className);
-	const [sz, setSz] = useState<number>(size);
-	const [tit, setTitle] = useState<string>(title);
-	useEffect(() => {
-		setPathd(getPathd(name));
-	}, [name]);
-	useEffect(() => {
-		setOpacity(fillOpacity);
-	}, [fillOpacity]);
-	useEffect(() => {
-		setColor(fillColor);
-	}, [fillColor]);
-	useEffect(() => {
-		setLink(href);
-	}, [href]);
-	useEffect(() => {
-		setTg(target);
-	}, [target]);
-	useEffect(() => {
-		setCname(className);
-	}, [className]);
-	useEffect(() => {
-		setSz(size);
-	}, [size]);
-	useEffect(() => {
-		setTitle(title);
-	}, [title]);
-	return (
-		<Suspense fallback={<p>Loading...</p>}>
-			<a
-				href={link}
-				style={{ textDecoration: "none", color: "currentcolor" }}
-				className={cname}
-				target={tg}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					width={sz}
-					height={sz}
-				>
-					<title>{tit}</title>
-					<path d={pathd} fill={color} fillOpacity={opacity} />
-				</svg>
-			</a>
-		</Suspense>
-	);
+  const [pathd, setPathd] = useState<string>(() => getPathd(name));
+  const [opacity, setOpacity] = useState<number>(fillOpacity);
+  const [color, setColor] = useState<string>(fillColor);
+  const [link, setLink] = useState<string>(href);
+  const [tg, setTg] = useState<string>(target);
+  const [cname, setCname] = useState<string>(className);
+  const [sz, setSz] = useState<number>(size);
+  const [tit, setTitle] = useState<string>(title);
+  useEffect(() => {
+    setPathd(getPathd(name));
+  }, [name]);
+  useEffect(() => {
+    setOpacity(fillOpacity);
+  }, [fillOpacity]);
+  useEffect(() => {
+    setColor(fillColor);
+  }, [fillColor]);
+  useEffect(() => {
+    setLink(href);
+  }, [href]);
+  useEffect(() => {
+    setTg(target);
+  }, [target]);
+  useEffect(() => {
+    setCname(className);
+  }, [className]);
+  useEffect(() => {
+    setSz(size);
+  }, [size]);
+  useEffect(() => {
+    setTitle(title);
+  }, [title]);
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <a
+        href={link}
+        style={{ textDecoration: "none", color: "currentcolor" }}
+        className={cname}
+        target={tg}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width={sz}
+          height={sz}
+        >
+          <title>{tit}</title>
+          <path d={pathd} fill={color} fillOpacity={opacity} />
+        </svg>
+      </a>
+    </Suspense>
+  );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,133 +1,136 @@
 export type IconName =
-	| "codepen"
-	| "codesandbox"
-	| "dev-to"
-	| "discord"
-	| "facebook"
-	| "gitbook"
-	| "github"
-	| "gitlab"
-	| "hashnode"
-	| "jsfiddle"
-	| "line"
-	| "linkedin"
-	| "mastodon"
-	| "medium"
-	| "messenger"
-	| "slack"
-	| "stackedit"
-	| "stackoverflow"
-	| "telegram"
-	| "viber"
-	| "wechat"
-	| "whatsapp"
-	| "wordpress"
-	| "youtube"
-	| "twitter-X"
-	| "rss"
-	| "next-js"
-	| "node-js"
-	| "notion"
-	| "npm"
-	| "numpy"
-	| "openai"
-	| "threads"
-	| "tailwind-css"
-	| "tiktok"
-	| "toml"
-	| "typescript"
-	| "ubuntu"
-	| "vercel"
-	| "vim"
-	| "vs-code"
-	| "vite"
-	| "vk"
-	| "yaml"
-	| "android"
-	| "angular"
-	| "apache"
-	| "apple"
-	| "biome"
-	| "bitbucket"
-	| "bluesky";
+  | "codepen"
+  | "codesandbox"
+  | "dev-to"
+  | "discord"
+  | "facebook"
+  | "gitbook"
+  | "github"
+  | "gitlab"
+  | "hashnode"
+  | "jsfiddle"
+  | "line"
+  | "linkedin"
+  | "mastodon"
+  | "medium"
+  | "messenger"
+  | "slack"
+  | "stackedit"
+  | "stackoverflow"
+  | "telegram"
+  | "viber"
+  | "wechat"
+  | "whatsapp"
+  | "wordpress"
+  | "youtube"
+  | "twitter-X"
+  | "rss"
+  | "next-js"
+  | "node-js"
+  | "notion"
+  | "npm"
+  | "numpy"
+  | "openai"
+  | "threads"
+  | "tailwind-css"
+  | "tiktok"
+  | "toml"
+  | "typescript"
+  | "ubuntu"
+  | "vercel"
+  | "vim"
+  | "vs-code"
+  | "vite"
+  | "vk"
+  | "yaml"
+  | "android"
+  | "angular"
+  | "apache"
+  | "apple"
+  | "biome"
+  | "bitbucket"
+  | "bluesky"
+  | "react"
+  | "markdown"
+  | "daisy-ui";
 
 export interface Icons {
-	name: IconName;
+  name: IconName;
 
-	d: string;
+  d: string;
 }
 
 export interface JsxSocialIconProps {
-	/**
-	 * ```
-	 * 'Name of icon'
-	 * ```
-	 */
-	name: Icons["name"];
-	// Anchor Element
-	/**
-	 * ```
-	 * 'HTML Anchor Element Attribute "href"'
-	 * ```
-	 *
-	 * ```html
-	 * <a href=""></a>
-	 *
-	 * ```
-	 */
-	href?: string;
-	/**
-	 * ```
-	 * 'HTML Anchor Element Attribute "target"'
-	 * ```
-	 *
-	 * ```html
-	 * <a href="" target=""></a>
-	 *
-	 * ```
-	 */
-	target?: "_blank" | "_parent" | "_self" | "_top";
-	/**
-	 * ```
-	 * 'HTML Anchor Element `className`'
-	 * ```
-	 *
-	 * ```
-	 * 'Default style - {textDecoration: none, color: currentcolor }'
-	 * ```
-	 *
-	 * ```html
-	 *
-	 * <a href="" target="" className=""></a>
-	 *
-	 * ```
-	 */
-	className?: string;
-	// Icon
-	/**
-	 * ```
-	 * 'Size for icon - default - 24'
-	 * ```
-	 */
-	size?: number;
-	/**
-	 * ```
-	 * 'Color for icon - default - "currentcolor"'
-	 * ```
-	 */
-	fillColor?: string;
-	/**
-	 * ```
-	 * 'Opacity of icon. 0.1 to 1 > default 0.7'
-	 * ```
-	 */
-	fillOpacity?: 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1;
-	/**
-	 * ```
-	 * 'Tooltip text for icon. default name of icon .'
-	 * ```
-	 *
-	 *
-	 */
-	title?: string;
+  /**
+   * ```
+   * 'Name of icon'
+   * ```
+   */
+  name: Icons["name"];
+  // Anchor Element
+  /**
+   * ```
+   * 'HTML Anchor Element Attribute "href"'
+   * ```
+   *
+   * ```html
+   * <a href=""></a>
+   *
+   * ```
+   */
+  href?: string;
+  /**
+   * ```
+   * 'HTML Anchor Element Attribute "target"'
+   * ```
+   *
+   * ```html
+   * <a href="" target=""></a>
+   *
+   * ```
+   */
+  target?: "_blank" | "_parent" | "_self" | "_top";
+  /**
+   * ```
+   * 'HTML Anchor Element `className`'
+   * ```
+   *
+   * ```
+   * 'Default style - {textDecoration: none, color: currentcolor }'
+   * ```
+   *
+   * ```html
+   *
+   * <a href="" target="" className=""></a>
+   *
+   * ```
+   */
+  className?: string;
+  // Icon
+  /**
+   * ```
+   * 'Size for icon - default - 24'
+   * ```
+   */
+  size?: number;
+  /**
+   * ```
+   * 'Color for icon - default - "currentcolor"'
+   * ```
+   */
+  fillColor?: string;
+  /**
+   * ```
+   * 'Opacity of icon. 0.1 to 1 > default 0.7'
+   * ```
+   */
+  fillOpacity?: 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1;
+  /**
+   * ```
+   * 'Tooltip text for icon. default name of icon .'
+   * ```
+   *
+   *
+   */
+  title?: string;
 }
